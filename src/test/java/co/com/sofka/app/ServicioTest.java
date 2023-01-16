a8620ebd97f881eba8de1e69099eb2bb02073473
Diego Gonzalez <diego.gonzalez@sofka.com.co>
2023-01-16 16:32:30 -0300

    Implementación de Tests básicos con StepVerifier

--- src/test/java/co/com/sofka/app/ServicioTest.java
+++ src/test/java/co/com/sofka/app/ServicioTest.java
@@ -1,4 +1,47 @@
 package co.com.sofka.app;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+@SpringBootTest
 public class ServicioTest {
+    @Autowired
+    Servicio servicio;
+
+    //Trabajando con Mono verifico un solo valor
+    @Test
+    void testMono(){
+        Mono<String> uno = servicio.buscarUno();
+        StepVerifier.create(uno).expectNext("Pedro").verifyComplete();
+    }
+
+    //Trabajando con Flux trabajo verificando un flujo de valores
+    @Test
+    void testVarios(){
+        Flux<String> uno = servicio.buscarTodos();
+        StepVerifier.create(uno).expectNext("Pedro").expectNext("Maria").expectNext("Jesus").expectNext("Carmen").verifyComplete();
+    }
+
+    //Trabajando con valores obtenidos en un determinado período de espera
+    @Test
+    void testVariosLento(){
+        Flux<String> uno = servicio.buscarTodosLento();
+
+        StepVerifier.create(uno)
+                .expectNext("Pedro")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Maria")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Jesus")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Carmen")
+                .thenAwait(Duration.ofSeconds(1)).verifyComplete();
+    }
 }
